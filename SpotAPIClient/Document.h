c0b6a3e9908c377242ca904e166c9fc0fda91bf9
Cam Saul <cam@geotip.com>
2014-08-14 10:49:42 -0700

    Initial Commit

---
+++ SpotAPIClient/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  SpotAPIClient
+//
+//  Created by Cam Saul on 8/14/14.
+//  Copyright (c) 2014 Spot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ SpotAPIClient/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SpotAPIClient
+//
+//  Created by Cam Saul on 8/14/14.
+//  Copyright (c) 2014 Spot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
